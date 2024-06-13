87d388f85309331fca8315e35589802f0aab9e7a
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-06-13 18:42:36 +0300

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <body className='text-ctp-text'>
+        <header className='flex justify-between items-center px-30px py-4'>
+            <a class="logo" href="/"><img src="../onion.jpeg" className='size-16 rounded-full' alt="logo"/></a>
+            <nav>
+                <ul className="flex">
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+            <a className="bg-ctp-green text-black px-8 py-2 rounded-full" href="#">Contact</a>
+            <p className="bg-ctp-green text-black px-8 py-2 rounded-full">Menu</p>
+        </header>
+    </body>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      gridTempleteColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "ctp",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "mocha",
+    }),
+  ],
+};
