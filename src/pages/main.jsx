a0ad6512c80577c0a6135fc1d28ddcee5cfe9bb6
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-08-06 01:49:42 +0300

    resolve empty pages

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "../style/index.css";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
 );
