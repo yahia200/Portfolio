51a4014cac15b6a27a66956ded6a5359da3927eb
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-08-06 03:55:39 +0300

    resolve take 4 re  asp problem

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../style/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter basename="/Portfolio">
+    <HashRouter basename="/Portfolio">
       <App />
-    </BrowserRouter>
+    </HashRouter>
 );
