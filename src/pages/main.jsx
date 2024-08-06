d07299ff95a943eafdea153216d3dde2254a3d5f
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-08-06 03:50:44 +0300

    resolve take 2 re 2 asp problem

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "../style/index.css";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/Portfolio">
       <App />
     </BrowserRouter>
 );
