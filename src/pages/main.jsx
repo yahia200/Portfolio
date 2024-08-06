4a224ac1682637da733d967ecf00751415a19070
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-08-06 03:45:13 +0300

    resolve take 2 re asp problem

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../style/index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <HashRouter basename="/">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
 );
