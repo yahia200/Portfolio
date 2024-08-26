9424837fe8a8df7332d32302cf5f72ad946631c1
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-08-26 15:07:39 +0300

    bg changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -15,7 +15,7 @@ const [color, setColor] = useState(0)
 const location = useLocation();
 let MINUTE_MS = 3000;
 let verticalDisplay = screen.width/screen.height < 1
-let s = [`transition-all duration-[3000ms] bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f5e0dc65_6px,#181825_1px)]" : "bg-[radial-gradient(#f5e0dc50_4px,#181825_1px)]"} bg-[size:72px_72px] bg-[center_top_0px]`,
+let s = [
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f2cdcd65_6px,#181825_1px)]" : "bg-[radial-gradient(#f2cdcd50_4px,#181825_1px)]"} bg-[size:69px_69px] bg-[center_top_23px]`,
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f5c2e765_6px,#181825_1px)]" : "bg-[radial-gradient(#f5c2e750_4px,#181825_1px)]"} bg-[size:66px_66px] bg-[center_top_46px]`,
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#cba6f765_6px,#181825_1px)]" : "bg-[radial-gradient(#cba6f750_4px,#181825_1px)]"} bg-[size:63px_63px] bg-[center_top_69px]`,
@@ -23,8 +23,15 @@ let s = [`transition-all duration-[3000ms] bg-ctp-mantle h-[100%] ${verticalDisp
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#eba0ac65_6px,#181825_1px)]" : "bg-[radial-gradient(#eba0ac50_4px,#181825_1px)]"} bg-[size:57px_57px] bg-[center_top_115px]`,
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#fab38765_6px,#181825_1px)]" : "bg-[radial-gradient(#fab38750_4px,#181825_1px)]"} bg-[size:54px_54px] bg-[center_top_138px]`,
 `transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f9e2af65_6px,#181825_1px)]" : "bg-[radial-gradient(#f9e2af50_4px,#181825_1px)]"} bg-[size:51px_51px] bg-[center_top_161px]`,
-`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#a6e3a165_6px,#181825_1px)]" : "bg-[radial-gradient(#a6e3a150_4px,#181825_1px)]"} bg-[size:48px_48px] bg-[center_top_184px]`,]
-
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#a6e3a165_6px,#181825_1px)]" : "bg-[radial-gradient(#a6e3a150_4px,#181825_1px)]"} bg-[size:48px_48px] bg-[center_top_184px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f9e2af65_6px,#181825_1px)]" : "bg-[radial-gradient(#f9e2af50_4px,#181825_1px)]"} bg-[size:51px_51px] bg-[center_top_161px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#fab38765_6px,#181825_1px)]" : "bg-[radial-gradient(#fab38750_4px,#181825_1px)]"} bg-[size:54px_54px] bg-[center_top_138px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#eba0ac65_6px,#181825_1px)]" : "bg-[radial-gradient(#eba0ac50_4px,#181825_1px)]"} bg-[size:57px_57px] bg-[center_top_115px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f38ba865_6px,#181825_1px)]" : "bg-[radial-gradient(#f38ba850_4px,#181825_1px)]"} bg-[size:60px_60px] bg-[center_top_92px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#cba6f765_6px,#181825_1px)]" : "bg-[radial-gradient(#cba6f750_4px,#181825_1px)]"} bg-[size:63px_63px] bg-[center_top_69px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f5c2e765_6px,#181825_1px)]" : "bg-[radial-gradient(#f5c2e750_4px,#181825_1px)]"} bg-[size:66px_66px] bg-[center_top_46px]`,
+`transition-all duration-[3000ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f2cdcd65_6px,#181825_1px)]" : "bg-[radial-gradient(#f2cdcd50_4px,#181825_1px)]"} bg-[size:69px_69px] bg-[center_top_23px]`,]
+console.log(location.pathname);
 React.useEffect(() => {
 
   const interval = setInterval(() => {
@@ -35,7 +42,7 @@ React.useEffect(() => {
   }, []);
   let boarder = verticalDisplay ? "border-[0vw]" : "border-[1.5vw]";
  let d =  <div className={`text-ctp-text h-[100vh] ${boarder} border-ctp-crust font-outfit font-semibold text-lg overflow-hidden`}>
- <div className={s[color]}>
+ <div className={location.pathname === "/gallery" ? `transition-all duration-[500ms] ease-linear bg-ctp-mantle h-[100%] ${verticalDisplay ? "" : "rounded-3xl"} flex justify-center flex-col items-center shadow-inner ${verticalDisplay ? "bg-[radial-gradient(#f5e0dc08_6px,#181825_1px)]" : "bg-[radial-gradient(#f5e0dc05_4px,#181825_1px)]"} bg-[size:48px_48px] bg-[center_top_184px]` : s[color]}>
    <NavBar/>
    <div className="text-ctp-text border-ctp-crust w-[100%] font-outfit font-semibold text-lg overflow-hidden ">
    {!verticalDisplay ? <Socials/> : <></>}
